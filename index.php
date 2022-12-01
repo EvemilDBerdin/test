6b3f665bb9cac545bd79ba6040e30d7685ff23cb
evemil <evemilberdin>
2022-12-01 16:43:05 +0800

    '2022/12/01'

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
     $datenow = date("Y/m/d");
     $test = shell_exec("git add .");
     $test2 = shell_exec("git commit -m '".$datenow."'");
-    $test3 = shell_exec("git push origin master");
+    $test3 = shell_exec("git push");
 
     if($test3){
         echo json_encode('success');
